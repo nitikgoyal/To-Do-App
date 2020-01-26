37dd53f6caac616ff0a96292a578d3dc8dba7467
nitikgoyal <nitikgoyal2712@gmail.com>
2020-01-26 15:07:01 +0530

    Angular To-DoApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To-Doapp';
+  storedPosts=[];
+
+  onPostAdded(post: any){
+    this.storedPosts.push(post);
+}
 }
